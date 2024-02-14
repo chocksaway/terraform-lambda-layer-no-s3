08ccd08cbaa7269944917b264d48dc234a252351
milesd <milesd@chocksaway.com>
2024-02-14 10:27:01 +0000

    Changing node version to node20.x

--- lambda.tf
+++ lambda.tf
@@ -4,7 +4,7 @@ resource "aws_lambda_function" "simple_node_lambda_function" {
   role          = aws_iam_role.simple-node-lambda_role.arn
   handler       = "index.handler"
   source_code_hash = filebase64sha256("lambda.zip")
-  runtime = "nodejs14.x"
+  runtime = "nodejs20.x"
   layers = [aws_lambda_layer_version.simple_node_lambda-layer.arn]
 }
 
